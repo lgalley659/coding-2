5109ebe8f76f8de813ab887c42e7c778a6f73901
lgalley659 <lgalley659@boyslatin.org>
2026-02-10 14:30:18 +0000

    discount

--- Unit 1/fundamentalReview.py
+++ Unit 1/fundamentalReview.py
@@ -1,1 +1,17 @@
-# Lumen G
+
+def discount():
+    itemPrice = input("Please enter the item price. ")
+    if itemPrice >= 50 or itemPrice <=75:
+        discount = .15
+        sum itemPrice * discount
+        total = itemPrice - sum
+        print("this is your final total "+ str(total))
+    elif itemPrice >75:
+        discount = .25
+        total = itemPrice * discount
+        total = itemPrice - sum
+        print("This is your final total "+ str(total))
+    else:
+        print("Sorry, you do not get a discount")
+        
+discount()
