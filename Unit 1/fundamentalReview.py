ed74179672c90fca4934074a378fb3f6282acb58
lgalley659 <lgalley659@boyslatin.org>
2026-02-11 14:35:56 +0000

    classwork

--- Unit 1/fundamentalReview.py
+++ Unit 1/fundamentalReview.py
@@ -15,3 +15,27 @@ def discount():
         print("Sorry, you do not get a discount")
         
 discount()
+
+def jobFairCheck(gpa , age):
+    if gpa >= 90 and age >= 17:
+        print("You meet the requirements for the fair")
+    else: 
+        print("You do not meet the requirements for the fair")
+
+def GPA():
+    gradeAverage = input('Enter your GPA')
+    if gradeAverage >= 90
+        print('You are qualified for the job fair')
+    else:
+        print("Not qualified")
+
+def age():
+    age = input("Enter your age")
+    if age >= 17
+        print("You are qualified for the job fair")
+    else:
+        ("You are too young for to attend the job fair")
+
+
+
+def gpa():
