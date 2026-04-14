48d2662ad5cce5003a631a5cf641d3367deb4383
lgalley659 <lgalley659@boyslatin.org>
2026-04-14 13:32:42 +0000

    work

--- Unit 3/placeholderAp.py
+++ Unit 3/placeholderAp.py
@@ -8,6 +8,31 @@ print(response.json())
 
 import requests
 
-url = 'https://bored-api.appbrewery.com/random'
+url = 'https://pokeapi.co/api/v2/pokemon-species/aegislash'
+reepose = requests.get(url)
 
-print(response.bored())
+print(response.poke())
+
+
+
+query = "https://pokeapi.co/v2/pokemon-species/mewtwo/"
+response = requests.get(query)
+print(response)
+print(response.json())
+
+ 
+if response == 200:
+    data= response.json()
+
+    filtered_data= {
+         "name": data["name"],
+         "height": data["height"],
+         "weight": data["weight"],
+         "types": data["types"],
+         "abilities": [ability["ability"]["name"] for ability in data["abilites"]]
+    }
+    
+    print(filtered_data)
+else:
+    print("data not found")
+
