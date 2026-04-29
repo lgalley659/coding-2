5e5043f01461c3e8f26617bc90ecac634576204d
lgalley659 <lgalley659@boyslatin.org>
2026-04-29 13:37:19 +0000

    sql

--- unit 4/sql_intro_p2.py
+++ unit 4/sql_intro_p2.py
@@ -1,14 +1,14 @@
 #1. import sqlite module to be able to use a database
-import sqlite
+import sqlite3
 
 # 2. the connect method starts our database
-connect = sqlite.connect()
+connect = sqlite3.connect()
 
 #3. the cursor variable creates a new object that lets us send objects to our database
 cursor = connect.cursor()
 
 #4. we need to create a schema (structure) for our data
-cursor.excute('''
+cursor.execute('''
     CREATE TABLE computers(
     id INTEGER PRIMARY KEY,
     model TEXT,
@@ -16,5 +16,13 @@ cursor.excute('''
     hasWebcam BOOLEAN,
     memory INTEGER
     price INTEGER
-    
+              
                )''')
+
+cursor.execute('''
+    INSERT INTO computers(mode, color, hasWebcam, memory, price)
+    VALIES('apple m4', 'blue', True, 8, 1500)
+              ''')
+
+connect.commit()
+connect.close()
