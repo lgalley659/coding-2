6e00536870f4dcc3f26fd9c3248edcedbd6dd7e0
lgalley659 <lgalley659@boyslatin.org>
2026-04-30 13:35:08 +0000

    sql

--- unit 4/sql_intro_p2.py
+++ unit 4/sql_intro_p2.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 # 2. the connect method starts our database
-connect = sqlite3.connect()
+connect = sqlite3.connect('testDb_p2.db')
 
 #3. the cursor variable creates a new object that lets us send objects to our database
 cursor = connect.cursor()
@@ -14,15 +14,33 @@ cursor.execute('''
     model TEXT,
     color TEXT,
     hasWebcam BOOLEAN,
-    memory INTEGER
+    memory INTEGER,
     price INTEGER
               
                )''')
 
 cursor.execute('''
-    INSERT INTO computers(mode, color, hasWebcam, memory, price)
-    VALIES('apple m4', 'blue', True, 8, 1500)
+    INSERT INTO computers(model, color, hasWebcam, memory, price)
+    VALUES('apple m4', 'blue', True, 8, 1500)
               ''')
 
+cursor.execute('''
+    INSERT INTO computers(model, color, hasWebcam, memory, price)
+    VALUES('apple m4', 'blue', True, 8, 1500)
+              ''')
+
+cursor.execute('''
+    INSERT INTO computers(model, color, hasWebcam, memory, price)
+    VALUES('apple m1', 'red', True, 8, 1500)
+              ''')
+
+
+#update a computer
+cursor.execute('''
+    UPDATE computers
+    SET color = 'yellow
+    WHERE id = 3           
+              ''')
+        
 connect.commit()
 connect.close()
